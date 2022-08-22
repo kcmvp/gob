8d6a2c0b8361c58f987d97d3044a5da0ed833704
kcmvp <kcheng.mvp@gmail.com>
2022-08-22 16:18:00 +0800

    refactor for be

--- cmd/run.go
+++ cmd/run.go
@@ -5,40 +5,39 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/kcmvp/gos/builder"
 	"github.com/spf13/cobra"
-	"log"
 )
 
-var (
-	// runCmd represents the run command
-	runCmd = &cobra.Command{
-		Use:       "run",
-		Short:     "run clean, test, lint and build against current project",
-		ValidArgs: []string{"clean", "test", "lint", "build"},
-		Args: func(cmd *cobra.Command, args []string) error {
-			err := cobra.MinimumNArgs(1)(cmd, args)
-			if err == nil {
-				err = cobra.OnlyValidArgs(cmd, args)
-			}
-			return err
-		},
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("%v \n", args)
-			var acts []builder.Action
-			for _, act := range args {
-				if a, ok := builder.ValueOf(act); ok {
-					if len(acts) > 0 && a == acts[len(acts)-1] {
-						log.Printf("ignore repeat action %s \n", a)
-					} else {
-						acts = append(acts, a)
-					}
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:       "run",
+	Short:     "run clean, test, lint and build against current project",
+	ValidArgs: []string{"clean", "test", "lint", "build"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		err := cobra.MinimumNArgs(1)(cmd, args)
+		if err == nil {
+			err = cobra.OnlyValidArgs(cmd, args)
+		}
+		return err
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("%v \n", args)
+		var acts []builder.Action
+		for _, act := range args {
+			if a, ok := builder.ValueOf(act); ok {
+				if len(acts) > 0 && a == acts[len(acts)-1] {
+					log.Printf("ignore repeat action %s \n", a)
+				} else {
+					acts = append(acts, a)
 				}
 			}
-			getBuilder(cmd).Run(acts...)
-		},
-	}
-)
+		}
+		getBuilder(cmd).Run(acts...)
+	},
+}
 
 func init() {
 	rootCmd.AddCommand(runCmd)
@@ -51,5 +50,5 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	//runCmd.Flags().StringSliceP("action", "a", []string{"build"}, "run project build action")
+	// runCmd.Flags().StringSliceP("action", "a", []string{"build"}, "run project build action")
 }
