00cf182c9f0946104c4ceb3920ff0f91076721f5
kcmvp <kcheng.mvp@gmail.com>
2023-12-25 09:13:59 +0800

    #14: comment out golangci-lint test

--- cmd/initializer.go
+++ cmd/initializer.go
@@ -6,12 +6,9 @@ package cmd
 import (
 	_ "embed"
 	"fmt"
-	"github.com/kcmvp/gb/cmd/shared"
 	"github.com/kcmvp/gb/internal"
 	"github.com/samber/lo"
 	"github.com/spf13/cobra"
-	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -27,16 +24,15 @@ var initializerFunc = func(_ *cobra.Command, _ []string) {
 		return strings.HasPrefix(plugin.D, golangCiLinter)
 	})
 	if !ok {
-		latest, err := shared.LatestVersion(golangCiLinter, "v1.55.*")
-		if err != nil {
-			latest = defaultVersion
-		}
-		internal.CurProject().InstallPlugin(fmt.Sprintf("%s@%s", golangCiLinter, latest), "lint", "run, ./...")
-		// generate .golangci.yaml
-		cfg := filepath.Join(internal.CurProject().Root(), ".golangci.yaml")
-		if _, err := os.Stat(cfg); err != nil {
-			os.WriteFile(cfg, golangci, 0666)
-		}
+		//latest, err := shared.LatestVersion(golangCiLinter, "v1.55.*")
+		//if err != nil {
+		//	latest = defaultVersion
+		//}
+		//internal.CurProject().InstallPlugin(fmt.Sprintf("%s@%s", golangCiLinter, latest), "lint", "run, ./...")
+		//cfg := filepath.Join(internal.CurProject().Root(), ".golangci.yaml")
+		//if _, err := os.Stat(cfg); err != nil {
+		//	os.WriteFile(cfg, golangci, 0666)
+		//}
 	}
 	internal.CurProject().Setup(true)
 }
