d7d9629ae947a7314fc20eff4c891706e4fedbd9
kcmvp <kcheng.mvp@gmail.com>
2024-01-16 00:52:28 +0800

    #35: progress bar support for long-running task

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/fatih/color v1.16.0
 	github.com/jedib0t/go-pretty/v6 v6.4.9
 	github.com/samber/lo v1.38.1
+	github.com/schollz/progressbar/v3 v3.14.1
 	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.17.0
 	github.com/stretchr/testify v1.8.4
@@ -23,10 +24,11 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -40,7 +42,8 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- shared/multiple_writer.go
+++ shared/multiple_writer.go
@@ -3,10 +3,14 @@ package shared
 import (
 	"bufio"
 	"fmt"
-	"github.com/fatih/color" //nolint
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/samber/lo"
 
 	"github.com/creack/pty"
 )
@@ -37,23 +41,46 @@ func StreamCmdOutput(cmd *exec.Cmd, file string) error {
 	// Create a regular expression to match color escape sequences
 	colorRegex := regexp.MustCompile(`\x1b\[[0-9;]*m`)
 	// Goroutine to remove color escape sequences, print the colored output, and write the modified output to the file
+	ch := make(chan string)
+	eof := false
 	go func() {
 		for scanner.Scan() {
-			line := scanner.Text()
-
-			fmt.Println(line)
-			// Remove color escape sequences from the line
-			lineWithoutColor := colorRegex.ReplaceAllString(line, "")
-			// Write the modified line to the file
-			_, err = log.WriteString(lineWithoutColor + "\n")
-			if err != nil {
-				fmt.Println("Error writing to file:", err)
-				break
+			line := strings.TrimSpace(scanner.Text())
+			if len(line) > 0 {
+				ch <- line
 			}
 		}
+		eof = true
 		if err = scanner.Err(); err != nil {
 			fmt.Println("Error reading output:", err)
 		}
 	}()
+	ticker := time.NewTicker(150 * time.Millisecond)
+	inProgress := false
+	progress := NewProgress()
+	for !eof {
+		select {
+		case line := <-ch:
+			progress.Reset()
+			lineWithoutColor := colorRegex.ReplaceAllString(line, "")
+			_, err = log.WriteString(lineWithoutColor + "\n")
+			line = lo.IfF(inProgress, func() string {
+				inProgress = false
+				return fmt.Sprintf("\r%-20s", line)
+			}).Else(line)
+			fmt.Println(line)
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				break
+			}
+		case <-ticker.C:
+			if !inProgress {
+				inProgress = true
+			}
+			_ = progress.Add(1)
+		}
+	}
+	_ = progress.Finish()
+	ticker.Stop()
 	return cmd.Wait()
 }
